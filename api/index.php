1de37a7b3b80851a79a40ecedcac00f72b48dc8b
tfigueira45 <110835436+tfigueira45@users.noreply.github.com>
2023-01-20 19:54:46 -0300

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,12 @@
+<?php
+    $dir = "imagens/"; 
+    // recebendo o arquivo multipart 
+    $file = $_FILES["arquivo"]; 
+    // Move o arquivo da pasta temporaria de upload para a pasta de destino 
+    if (move_uploaded_file($file["tmp_name"], "$dir/".$file["name"])) { 
+        echo "Arquivo enviado com sucesso!"; 
+    } 
+    else { 
+        echo "Erro, o arquivo n&atilde;o pode ser enviado."; 
+    }           
+?>
